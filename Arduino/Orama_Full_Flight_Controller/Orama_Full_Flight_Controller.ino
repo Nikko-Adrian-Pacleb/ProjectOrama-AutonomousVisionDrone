84177cfe13a0440dc1727bb62b76cd323a92f1ce
eluluke <122318824+eluluke@users.noreply.github.com>
2025-07-10 15:28:00 +0900

    worked on the full program on the plane
    added a todo list

--- Arduino/Orama_Full_Flight_Controller/Orama_Full_Flight_Controller.ino
+++ Arduino/Orama_Full_Flight_Controller/Orama_Full_Flight_Controller.ino
@@ -1,8 +1,16 @@
+//TODO: need to write a function for the motor PID control.
+//      Need to implement the LiDAR sensor into the code for automatic landing sequence. // STARTED
+//      Need to implement the code for object avoidance using the TOF sensors.
+
+// dynamic memory is looking okay. I think we should be fine.
+
 #include <Wire.h>       // 5% of dynamic memory (113 bytes)
 #include <Servo.h>      // 2% of dynamic memory (37 bytes)
+#include <SoftwareSerial.h>   //header file of software serial port
 #include "REG.h"        
 #include "wit_c_sdk.h"  
 #include "VL53L1X.h"
+
 
 // ###################### DEBUG VARIABLE ########################
 bool DEBUG = false;
@@ -10,6 +18,15 @@ bool DEBUG = false;
 
 // TOF Object initilization
 VL53L1X sensors[4];
+
+// LiDAR variables and softwareserial object
+SoftwareSerial Serial1(2, 3); //define software serial port name as Serial1 and define pin2 as RX & pin3 as TX
+ 
+int dist;                     //actual distance measurements of LiDAR
+int strength;                 //signal strength of LiDAR
+int check;                    //save check value
+int uart[9];                   //save data measured by LiDAR
+const int HEADER = 0x59;      //frame header of data package
 
 Servo gimbalServo;  // use 264 bytes program space and 4 bytes of SRAM
 #define SERVO_PIN 13
@@ -29,6 +46,12 @@ static char s_cDataUpdate = 0, s_cCmd = 0xff;
 #define BACK_RIGHT_MOTOR 5
 #define MANUAL_SWITCH 6
 
+// control the ESC using servo objects and write microseconds to them.
+Servo FrontLeftProp;
+Servo FrontRightProp;
+Servo BackLeftProp;
+Servo BackRightProp;
+
 // Pins for the reciever
 #define CH1 7
 #define CH2 8
@@ -37,6 +60,14 @@ static char s_cDataUpdate = 0, s_cCmd = 0xff;
 #define CH5 11
 #define CH6 12
 
+
+// data that will get recieved from the RPi and is used to control the drone based on what the computer vision sees.
+struct controlData {
+  int throttle; // a potentiometer value like 0 - 1684 or whatever that value is.
+  int switchMode; // use as a boolean so either 0 or 1
+  // have to think about what else to put in the struct.
+
+};
 
 // variables used to replace all delay functions and just use deltaTime
 unsigned long lastSensorRead = 0;
@@ -61,9 +92,8 @@ int16_t rollAngle = 0;      // tenths of degrees
 int16_t smoothedAngle = 0;  // exponential smoothing
 
 //TOF Variables
-int16_t TOFData[] = {0, 0, 0, 0};
-const uint8_t sensorAddresses[4] = {0x30, 0x31, 0x32, 0x33};
-const uint8_t xshutPins[4] = {2, 3, 4, 5}; // Pins connected to XSHUT
+int16_t TOFData[] = {0, 0, 0, 0}; // not sure how nikko wanted to put this into a struct. It would be very similar.
+const uint8_t sensorAddresses[4] = {0x30, 0x31, 0x32, 0x33}; // these will be found after hooking up the I2C multiplexer.
 
 
 void setup() {
@@ -114,7 +144,10 @@ void loop() {
     s_cDataUpdate = 0;
   }
 
+  if 
+
 }
+
 
 // --------------- IMU Functions --------------- //
 static void AutoScanSensor(void) {
@@ -140,11 +173,11 @@ static void AutoScanSensor(void) {
   Serial.print("please check your connection\r\n");
 }
 
-static void CopeSensorData(uint32_t uiReg, uint32_t uiRegNum) { // Doesn't use any SRAM or program space currently
+static void CopeSensorData(uint32_t uiReg, uint32_t uiRegNum) { 
   if (uiReg == Yaw) s_cDataUpdate |= ANGLE_UPDATE;
 }
 
-static int32_t IICreadBytes(uint8_t dev, uint8_t reg, uint8_t *data, uint32_t length) { // Doesn't use any SRAM or program space currently
+static int32_t IICreadBytes(uint8_t dev, uint8_t reg, uint8_t *data, uint32_t length) {
   Wire.beginTransmission(dev);
   Wire.write(reg);
   if (Wire.endTransmission(false) != 0) return 0;
@@ -157,7 +190,7 @@ static int32_t IICreadBytes(uint8_t dev, uint8_t reg, uint8_t *data, uint32_t le
   return 1;
 }
 
-static int32_t IICwriteBytes(uint8_t dev, uint8_t reg, uint8_t *data, uint32_t length) { // Doesn't use any SRAM or program space currently
+static int32_t IICwriteBytes(uint8_t dev, uint8_t reg, uint8_t *data, uint32_t length) { 
   Wire.beginTransmission(dev);
   Wire.write(reg);
   Wire.write(data, length);
@@ -174,7 +207,7 @@ int16_t smoothAngle(int16_t newAngle) {
    return (int16_t)((3L * newAngle + 7L * smoothedAngle) / 10L);  // alpha = 0.3
 }
 
-int16_t pidControl(int16_t target, int16_t current) { // Doesn't use any SRAM or program space currently
+int16_t pidControl(int16_t target, int16_t current) {
   unsigned long now = millis();
   uint16_t dtMs = now - lastTime;
   if (dtMs == 0) dtMs = 1;
@@ -191,33 +224,32 @@ int16_t pidControl(int16_t target, int16_t current) { // Doesn't use any SRAM or
 
   return (int16_t)output;
 }
+// --------------- Motor Control Functions --------------- //
 
-// --------------- TOF sensor Functions --------------- //
+
+/** Starts all 4 ESC's so that you can actually control them.
+ * void function
+*/
+void motor_init() {
+  FrontLeftProp.attach(FRONT_LEFT_MOTOR);
+  FrontRightProp.attach(FRONT_RIGHT_MOTOR);
+  BackLeftProp.attach(BACK_LEFT_MOTOR);
+  BackRightProp.attach(BACK_RIGHT_MOTOR);
+
+  FrontLeftProp.writeMicroseconds(1000);
+  FrontRightProp.writeMicroseconds(1000);
+  BackLeftProp.writeMicroseconds(1000);
+  BackRightProp.writeMicroseconds(1000);
+}
+
+int16_t motorPIDControl(int16_t target, int16_t current) { // need to adapt it to 3 axis and 4 motors instead of just 1 axis and 1 motor.
+  
+}
+
+
+// --------------- TOF Sensor Functions --------------- //
 void sensor_init(VL53L1X::DistanceMode range_mode, bool high_speed, VL53L1X sensor) {
   Wire.begin();
-
-  // Disable all sensors first
-  for (int i = 0; i < 4; i++) {
-    pinMode(xshutPins[i], OUTPUT);
-    digitalWrite(xshutPins[i], LOW); // Pull XSHUT low to disable sensor
-  }
-  delay(10);
-
-  // Initialize sensors one at a time
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(xshutPins[i], HIGH); // Enable one sensor
-    delay(10); // Allow time to boot
-
-    sensors[i].setTimeout(500);
-    if (!sensors[i].init()) {
-      Serial.print("Failed to detect sensor ");
-      Serial.println(i);
-      while (1);
-    }
-
-    sensors[i].setAddress(sensorAddresses[i]);
-  }
-
   // Start ranging on all sensors
   for (int i = 0; i < 4; i++) {
     sensors[i].startContinuous(50); // 50ms interval
@@ -231,6 +263,76 @@ uint16_t* getAllTOFSensorData() {
   return TOFData;
 }
 
+// --------------- LiDAR Sensor Functions --------------- //
+
+
+/** Starts and connects the LiDAR through UART.
+ * @return int available - number that is either 0 or 1 to tell if the serial is off or running.
+*/
+int lidar_init() {
+  Serial1.begin(115200);
+
+  if(Serial1.available()) {
+    if (Serial1.read() == HEADER) {
+      uart[0] = HEADER;
+      if (Serial1.read() == HEADER) {
+        uart[1] = HEADER;
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+
+int getLiDARData() {
+  if(Serial1.available()) {
+    if (Serial1.read() == HEADER) {
+      uart[0] = HEADER;
+      if (Serial1.read() == HEADER) {
+        uart[1] = HEADER;
+        for (int i = 2; i < 9; i++) uart[i] = Serial1.read();
+        for (int i = 0; i < 8; i++) check += uart[i];
+        if (uart[8] == (check & 0xff)) {
+          dist = uart[2] + uart[3] * 256;
+          //strength = uart[4] + uart[5] * 256;  //if we want to use this we can but right now just using the distance.
+        }
+      }
+    }
+  }
+  return dist;
+}
+
+
+
+/** Automatically lands the drone using the LiDAR sensor. 
+  * @return void
+*/
+void autoLand() {
+
+ /* 1. need to first check the distance that the drone is currently from the ground.
+    2. If the distance is really large start descending at fastest speed allowed.
+    3. If not then use the percentage to find the descending speed.
+    4. then write the correct speed to the motors and constantly adjusting till the distance stops changing.
+    5. once the distance stops changing stop the motors.
+
+    //coudln't do any reasearch or testing on this so someone will have to test and see if it works.
+  */
+
+  //almost positive the way that this works is not correct. but I hope you get the idea.
+  
+  int groundDist = getLiDARData();
+  int lastDist = 0;
+  if (groundDist > 3000) {
+    //need to write to the motors through the PID to make sure that it lands flat.
+    //not quite sure what value will start to lower the drone. This has to be found once we have the total weight and have tried flying it on a string or something once the PID works.
+    int lastDist = getLiDARData();
+  }
+  while (groundDist > 20) { // need to write it so that it uses a last distance to make sure that the drone is still descending.
+
+  }
+  
+
+}
 
 
 
